a148cbdec06781f201f3f2c8b564c7a0357da089
Moksha Vardhan <mokshamny333@gmail.com>
2026-03-04 11:54:00 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
+import "./styles.css";
 
 export const metadata: Metadata = {
   title: "Komarapuri Moksha Vardhan - Portfolio",
